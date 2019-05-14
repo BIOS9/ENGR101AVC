7e69db1512aba3515866b7b8393fd94b6be7f628
NightFish <NightFish.net@gmail.com>
2019-05-14 16:25:02 +1200

    Removes inline implementation of logger

---
+++ software/src/logger.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "logger.h"
+
+const char * LogPrefixes[] = { "DEBUG", "WARNING", "INFO", "ERROR" };
+
+/**
+ * Logs messages and errors to console.
+ * 
+ * Supports variable argument formatting
+ */
+void logMsg(const char *format, const char *source, LogLevel logLevel, ...) {
+    // If the message priority is less than the current priority, discard the message
+	if (CURRENT_LOG_LEVEL > logLevel)
+		return;
+
+	FILE *stream = (logLevel == ERROR) ? stderr : stdout; // If it's an error, print to stderr
+
+	printf("[%s][%s] ", LogPrefixes[logLevel], source); // Print the log level and log source
+
+	va_list argptr;
+
+	va_start(argptr, logLevel); // Get the argument list pointer
+	
+	vfprintf(stream, format, argptr); // Pass arguments to vprintf to print messages
+	va_end(argptr);
+
+	printf("\n"); // End the line
+}
--- software/src/logger.h
+++ software/src/logger.h
@@ -7,11 +7,6 @@
 
 #define CURRENT_LOG_LEVEL DEBUG // CHANGE THIS TO CHANGE LOGGING LEVEL
 
-#include <stdio.h>
-#include <stdarg.h>
-
-#define CURRENT_LOG_LEVEL DEBUG // CHANGE THIS TO CHANGE LOGGING LEVEL
-
 enum LogLevel {
 	DEBUG = 0,
 	WARNING = 1,
@@ -19,31 +14,6 @@ enum LogLevel {
 	ERROR = 3
 };
 
-const char * LogPrefixes[] = { "DEBUG", "WARNING", "INFO", "ERROR" };
-
-/**
- * Logs messages and errors to console.
- * 
- * Supports variable argument formatting
- */
-void logMsg(const char *format, const char *source, LogLevel logLevel, ...) {
-
-	// If the message priority is less than the current priority, discard the message
-	if (CURRENT_LOG_LEVEL > logLevel)
-		return;
-
-	FILE *stream = (logLevel == ERROR) ? stderr : stdout; // If it's an error, print to stderr
-
-	printf("[%s][%s] ", LogPrefixes[logLevel], source); // Print the log level and log source
-
-	va_list argptr;
-
-	va_start(argptr, logLevel); // Get the argument list pointer
-	
-	vfprintf(stream, format, argptr); // Pass arguments to vprintf to print messages
-	va_end(argptr);
-
-	printf("\n"); // End the line
-}
+void logMsg(const char *format, const char *source, LogLevel logLevel, ...);
 
 #endif
