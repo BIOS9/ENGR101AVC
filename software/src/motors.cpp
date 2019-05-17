6b8a80114ffef2cc509028e5b249697cd68b8fa4
NightFish <NightFish.net@gmail.com>
2019-05-17 14:51:51 +1200

    Fixes lerp function

--- software/src/motors.cpp
+++ software/src/motors.cpp
@@ -33,14 +33,14 @@ void Motors::SetMotorSpeed(Motor motor, int speed) {
 
 // Got this function off of stackoverflow
 float Motors::lerp(float a, float b, float fraction) {
-    return (a * (1.0 - fraction)) + (b * f);
+    return (a * (1.0 - fraction)) + (b * fraction);
 }
 
 int Motors::interpolateMotorSpeed(int value) {
     if(value > 0)
-        return (int)lerp(STOP_MOTOR_SPEED, MAX_MOTOR_SPEED, float(value) / 100f);
+        return (int)lerp(STOP_MOTOR_SPEED, MAX_MOTOR_SPEED, float(value) / 100.0);
     else if(value < 0)
-        return (int)lerp(MIN_MOTOR_SPEED, STOP_MOTOR_SPEED, float(100 - value) / 100f);
+        return (int)lerp(MIN_MOTOR_SPEED, STOP_MOTOR_SPEED, float(100 - value) / 100.0);
     else
         return STOP_MOTOR_SPEED;
 }
