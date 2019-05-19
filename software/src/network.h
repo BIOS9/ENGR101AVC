af14046ec8658840d1208c8322f591b9e373474c
Alexandra-Sparrow0201 <35312741+Alexandra-Sparrow0201@users.noreply.github.com>
2019-05-20 10:16:49 +1200

    Created network.h
    Code copied from Mathew. The Network  class used to connect to the network gate and open it.

---
+++ software/src/network.h
@@ -0,0 +1,20 @@
+/**
+ * Network class to connect to the network gate and open it.
+ *Produced by Matthew
+ */
+
+#ifndef NETWORK_H // Header guard
+#define NETWORK_H
+
+class Network {
+public:
+    Network(char *ip, int port); // Constructor
+    ~Network(); // Destructor
+    void OpenGate();
+
+private:
+    char * IPAddr;
+    int portNumber;
+};
+
+#endif // End of header guard
