d29322a032669fbd4d997c3193b3ab12d7d175ae
NightFish <NightFish.net@gmail.com>
2019-05-27 11:05:02 +1200

    Fixes error in network

--- software/src/network.cpp
+++ software/src/network.cpp
@@ -4,7 +4,7 @@
 
 Network::Network(char *ip, int port){
   logMsg("Network loading", "Network", DEBUG);
-  IPAdd = ip; //IP address
+  IPAddr = ip; //IP address
   portNumber = port; //Port Number
 }
 
@@ -12,7 +12,7 @@ void Network::OpenGate(){
   logMsg("Opening gate...", "Network", INFO); //Message to announce begining of opening gate
   logMsg("Connecting to server...", "Network", DEBUG);
 
-  connect_to_server(IPAdd, portNumber); // Connect by sending IP adress and port number that we want to connect to
+  connect_to_server(IPAddr, portNumber); // Connect by sending IP adress and port number that we want to connect to
 
   char message[24];//To save message
 
